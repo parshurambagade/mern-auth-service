fa35cba38f9f312e619c58fa8d0912d8b4bdb18a
parshurambagade <parshurambagade855@gmail.com>
2026-02-06 22:33:34 +0530

    chore: update ESLint configuration, enhance server startup logic, and modify package scripts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
 import { ENV } from "./config/env.js";
+import app from "./app.js";
 
-console.log("Server is starting at port: ", ENV.PORT);
+const startServer = () => {
+  const PORT = ENV.PORT || 3000;
+
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
