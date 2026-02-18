2cefa47b2a5d9d37137be85eb93af18e0c48a93a
parshurambagade <parshurambagade855@gmail.com>
2026-02-18 22:39:55 +0530

    chore: update environment configuration by adding database variables and prepare different env files for different environments

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,10 +1,23 @@
 import { config } from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const { PORT, NODE_ENV } = process.env;
+config({
+  path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+});
+
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+  process.env;
 
 export const ENV = {
   PORT,
   NODE_ENV,
+  DB_HOST,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
 };
