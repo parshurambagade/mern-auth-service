2ce8673a131d7fd20d22e0508590521f39fb3b5f
parshurambagade <parshurambagade855@gmail.com>
2026-02-26 19:04:40 +0530

    feat: enhance server startup process to include database connection and await server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
 import app from "./app.js";
 import { ENV } from "./config/env.js";
 import logger from "./config/logger.js";
+import { prisma } from "./config/prisma.js";
 
-const startServer = () => {
-  const PORT = ENV.PORT || 3000;
+const startServer = async () => {
+    const PORT = ENV.PORT || 3000;
 
-  try {
-    app.listen(PORT, () => {
-      logger.info("✅ Server is running...", { PORT });
-    });
-  } catch (error) {
-    console.error("Error starting the server:", error);
-    process.exit(1);
-  }
+    try {
+        await prisma.$connect();
+        logger.info("✅ Database connected");
+
+        app.listen(PORT, () => {
+            logger.info("✅ Server is running...", { PORT });
+        });
+    } catch (error) {
+        console.error("Error starting the server:", error);
+        process.exit(1);
+    }
 };
 
-startServer();
+await startServer();
